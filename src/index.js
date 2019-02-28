5f3172fcfc2304dd33efc7d724f4880c0c56ae9c
Rafael <rafa.comp@gmail.com>
2019-02-28 17:17:06 -0300

    estilizacao

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Button from './Button'
 
+import './style.scss'
+
 class App extends Component {
 
   state = {
@@ -15,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello Rafadevx</h1>
+        <h1>Hello rafadevx</h1>
         <h2>{this.state.counter}</h2>
         <Button onClick={this.handleClick}>Somar</Button>
       </div>
