da74f0e07a3be32d5dd55594da3b905f2fdb0024
Rafael <rafa.comp@gmail.com>
2019-02-28 16:22:48 -0300

    defaultProps e propType e configuração do plugin do babel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { render } from 'react-dom'
 
 class Button extends Component {
+  static defaultProps = {
+    children: 'Salvar'
+  }
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  }
   render() {
     return <a href="" onClick={this.props.onClick} >{this.props.children}</a>
   }
 }
+
 
 class App extends Component {
 
